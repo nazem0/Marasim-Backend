58d0d77fff93a1b66b3995df864eab35f12fe883
Samer Said <supsamer2009@icloud.com>
2023-10-25 20:10:52 +0300

    PostViewModels

---
+++ ViewModels/PostViewModels/PostViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Models;
+
+namespace ViewModels.PostViewModels
+{
+	public class PostViewModel
+	{
+        public int ID { get; set; }
+        public required int VendorID { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required DateTime DateTime { get; set; }
+        public int? ServiceID { get; set; } = null;
+        public ICollection<PostAttachment>?  PostAttachments { get; set; }
+        public ICollection<Comment>? Comment { get; set; }
+        public ICollection<React>? React { get; set; }
+    }
+}
+
