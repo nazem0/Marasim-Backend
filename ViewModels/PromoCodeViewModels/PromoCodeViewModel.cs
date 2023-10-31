ade13b606e093c23e12956fed45715b11920e7d9
nazem0 <mohamednazem09@gmail.com>
2023-10-31 10:49:36 +0200

    Nullables

--- ViewModels/PromoCodeViewModels/PromoCodeViewModel.cs
+++ ViewModels/PromoCodeViewModels/PromoCodeViewModel.cs
@@ -2,7 +2,7 @@
 {
     public class PromoCodeViewModel
     {
-        public string Code { get; set; }
+        public required string Code { get; set; }
         public float Discount { get; set; }
         //public int Limit { get; set; }
         public int Count { get; set; }
