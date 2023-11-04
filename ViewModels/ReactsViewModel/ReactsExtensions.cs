995a884b550af56936edb9e1cd9774ced49b5d52
Samer Said <supsamer2009@icloud.com>
2023-11-04 20:21:54 +0200

    Swagger auth, React Controller

---
+++ ViewModels/ReactsViewModel/ReactsExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Models;
+
+namespace ViewModels.ReactsViewModel
+{
+	public static class ReactsExtensions
+	{
+        public static React ToModel(this AddReactViewModel AddReact, string UserID)
+        {
+            return new React
+            {
+                PostID = AddReact.PostID,
+                DateTime = AddReact.DateTime,
+                UserID = UserID
+            };
+        }
+    }
+}
+
