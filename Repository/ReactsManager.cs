eb37e5d56200f352922577e9da6c7c3a5417f664
Samer Said <supsamer2009@icloud.com>
2023-11-04 16:26:36 +0200

    React Controller

---
+++ Repository/ReactsManager.cs
@@ -0,0 +1,22 @@
+﻿using Models;
+
+namespace Repository
+{
+    public class ReactsManager : MainManager<React>
+    {
+        public ReactsManager(EntitiesContext _dBContext) : base(_dBContext) { }
+
+        public IQueryable<React> GetByPostID(int PostID)
+        {
+            return Get().Where(r => r.PostID == PostID);
+        }
+
+        public int CountReacts(int PostID)
+        {
+            return Get().Where(r => r.PostID == PostID).Count();
+        }
+
+
+
+    }
+}
