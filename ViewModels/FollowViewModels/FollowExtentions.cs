23ae8825b1ea5d63da34573a50048d98daeb6e8e
Mahmoud <mahmoud.shallaly@gmail.com>
2023-11-07 21:50:34 +0200

    add-delete-follow

---
+++ ViewModels/FollowViewModels/FollowExtentions.cs
@@ -0,0 +1,37 @@
+﻿using Models;
+using ViewModeles;
+using ViewModels.PostViewModels;
+
+namespace ViewModels.PostViewModels
+{
+    public static class FollowExtentions
+    {
+    
+
+    
+
+        public static Follow ToEntity(this AddFollowViewModel addFollowViewModel, string UserId)
+        {
+            return new Follow
+            {
+                UserId = UserId,
+                VendorId = addFollowViewModel.VendorId,
+            };
+        }
+
+
+        public static FollowViewModel ToViewModel(this Follow follow)
+        {
+            return new FollowViewModel
+            {
+                Id = follow.Id,
+                UserId = follow.UserId,
+                VendorId = follow.VendorId,
+            };
+        }
+    }
+}
+
+
+    
+  
