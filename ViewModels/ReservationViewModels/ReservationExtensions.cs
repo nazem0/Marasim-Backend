c8727a55d0345320fc4fc3482cfc3eb18e041cb2
Shallaly95 <mahmoud.shallaly@gmail.com>
2023-11-11 05:23:43 +0200

    merge

--- ViewModels/ReservationViewModels/ReservationExtensions.cs
+++ ViewModels/ReservationViewModels/ReservationExtensions.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ViewModels.ServiceViewModels;
 using ViewModels.UserViewModels;
 using ViewModels.VendorViewModels;
 
