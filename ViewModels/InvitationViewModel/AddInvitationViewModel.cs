c189cb9fa76211e645ba8b73d5fd4dcb323d057a
nazem0 <mohamednazem09@gmail.com>
2023-11-12 18:54:37 +0200

    Add/Get Invitation

---
+++ ViewModels/InvitationViewModel/AddInvitationViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels.InvitationViewModel
+{
+    public class AddInvitationViewModel
+    {
+        [Required(ErrorMessage = "اسم العريس مطلوب")]
+        [MaxLength(150, ErrorMessage = "اسم العريس لا يمكن أن يكون أطول من 150 حرفًا")]
+        public required string GroomName { get; set; }
+
+        [Required(ErrorMessage = "صورة العريس مطلوبة")]
+        public required IFormFile GroomPic { get; set; }
+        public string? GroomPicUrl { get; set; }
+
+        [Required(ErrorMessage = "اسم العروسة مطلوب")]
+        [MaxLength(150, ErrorMessage = "اسم العروسة لا يمكن أن يكون أطول من 150 حرفًا")]
+        public required string BrideName { get; set; }
+
+        [Required(ErrorMessage = "صورة العروسة مطلوبة")]
+        public required IFormFile BridePic { get; set; }
+        public string? BridePicUrl { get; set; }
+
+        [Required(ErrorMessage = "تاريخ الزفاف مطلوب")]
+        [DataType(DataType.Date, ErrorMessage = "يجب أن يكون تاريخ الزفاف تاريخًا صحيحًا")]
+        public required DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "صورة الغلاف مطلوبة")]
+        public required IFormFile Poster { get; set; }
+        public string? PosterUrl { get; set; }
+
+
+        [Required(ErrorMessage = "الوصف مطلوب")]
+        [MaxLength(150, ErrorMessage = "الوصف لا يمكن أن يكون أطول من 150 حرف")]
+        public required string Location { get; set; }
+    }
+}
---
+++ ViewModels/InvitationViewModel/InvitationExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels.InvitationViewModel
+{
+    public static class InvitationExtensions
+    {
+        public static Invitation ToInvitation(this AddInvitationViewModel Data, string UserId)
+        {
+            return new Invitation
+            {
+                UserId = UserId,
+                BrideName = Data.BrideName,
+                BridePicUrl = Data.BridePicUrl,
+                Date = Data.Date,
+                Location = Data.Location,
+                GroomName = Data.GroomName,
+                GroomPicUrl = Data.GroomPicUrl,
+                PosterUrl = Data.PosterUrl,
+            };
+        }
+        public static InvitationViewModel ToInvitationViewModel(this Invitation Data)
+        {
+            return new InvitationViewModel
+            {
+                UserId = Data.UserId,
+                BrideName = Data.BrideName,
+                BridePicUrl = Data.BridePicUrl,
+                Date = Data.Date,
+                GroomName = Data.GroomName,
+                GroomPicUrl = Data.GroomPicUrl,
+                PosterUrl = Data.PosterUrl,
+                Location = Data.Location
+            };
+        }
+    }
+}
