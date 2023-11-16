13bfadcae48dfc390df8f9bcd3485aeca7a58ab1
nazem0_home_pc <mohamednazem09@gmail.com>
2023-11-16 18:56:33 +0200

    adjusted roles seeding

--- Models/EntitiesContext.cs
+++ Models/EntitiesContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Models
 {
@@ -58,11 +59,11 @@ namespace Models
                 new Category { Id = 7, Name = "كوافير للرجال" },
                 new Category { Id = 8, Name = "كوافير للسيدات" }
             );
-
+            // Added Ids to prevent deleteing them each migration. :)
             modelBuilder.Entity<IdentityRole>().HasData(
-                new IdentityRole { Name = "user", NormalizedName = "USER" },
-                new IdentityRole { Name = "vendor", NormalizedName = "VENDOR" },
-                new IdentityRole { Name = "admin", NormalizedName = "ADMIN" });
+                new IdentityRole { Id = "12ff2abd-866c-457e-b725-8dad45ef4885", Name = "user", NormalizedName = "USER" },
+                new IdentityRole { Id = "3a590a5a-90de-4886-bc76-69dc9e138b44", Name = "vendor", NormalizedName = "VENDOR" },
+                new IdentityRole { Id = "8433688c-a3d9-442b-8e2b-31cc61ca02c2", Name = "admin", NormalizedName = "ADMIN" });
 
             modelBuilder.Entity<Governorate>().HasData(
                 new Governorate { Id = 1, NameAr = "القاهرة", NameEn = "Cairo" },
