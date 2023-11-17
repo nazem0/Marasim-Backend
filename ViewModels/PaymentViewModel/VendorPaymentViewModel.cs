438cc11422ed35725b338c197d6f51728e46e3d0
nazem0_home_pc <mohamednazem09@gmail.com>
2023-11-17 06:15:39 +0200

    Get Vendor Payments While Price is double to get precise number

--- ViewModels/PaymentViewModel/VendorPaymentViewModel.cs
+++ ViewModels/PaymentViewModel/VendorPaymentViewModel.cs
@@ -10,7 +10,7 @@ namespace ViewModels.PaymentViewModel
 {
     public class VendorPaymentViewModel
     {
-        public required float Price { get; set; }
+        public required double Price { get; set; }
         public required DateTime DateTime { get; set; }
         public required string ServiceTitle { get; set; }
         public required UserMinInfoViewModel User { get; set; }
