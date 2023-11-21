098ab2cd6215ee769fb7fc60ec04f2ff3bfaf8fc
nazem0_home_pc <mohamednazem09@gmail.com>
2023-11-21 13:07:43 +0200

    separate reacts & comments from post

--- ViewModels/PostViewModels/PostViewModel.cs
+++ ViewModels/PostViewModels/PostViewModel.cs
@@ -11,8 +11,8 @@ namespace ViewModels.PostViewModels
         public required string Description { get; set; }
         public required DateTime DateTime { get; set; }
         public required IEnumerable<PostAttachmentViewModel> PostAttachments { get; set; }
-        public IEnumerable<CommentViewModel>? Comments { get; set; }
-        public IEnumerable<ReactViewModel>? Reacts { get; set; }
+        public int CommentsCount { get; set; }
+        public int ReactsCount { get; set; }
         public required string VendorName { get; set; }
         public required string VendorPicUrl { get; set; }
         public required string VendorUserId { get; set; }
